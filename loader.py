32c7ae8c421ee83e05d6ff34949ec06dad119692
Sadikov Dev <ravshan4ikov@gmail.com>
2023-11-08 11:30:50 +0500

    edit bot token

--- loader.py
+++ loader.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 import pymongo
 
-bot = Bot(token="6838467582:AAFSO4HfTjwR3huucLhLdIjP5TQ1K6aN2E4", parse_mode=ParseMode.HTML)
+bot = Bot(token="6766715274:AAGRMYuQVlLDyA9g5GAn1pBMI5sgLWQMex0", parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 
 db_client = pymongo.MongoClient("mongodb+srv://iRavshan:Qarshi-2002@cluster0.umrxpw4.mongodb.net/")
